4e33c2495fb15c04478710611e87f2ee7451c459
dmatr <matros@bugfreakmedia.com>
2021-09-24 12:56:21 +0300

    added config.php.example

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require_once 'vendor/autoload.php';
 require_once 'sync.php';
 try {
+    if (!file_exists('config.php'))  throw new Exception("Rename config.php.example to config.php and set your googleDriveFolderId");
     $config = include 'config.php';
     new sync($config['syncFolder'], $config['googleDriveFolderId']);
 } catch (\Throwable $t) {
